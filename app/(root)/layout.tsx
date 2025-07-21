519711041660975a9d7cb37e45af77e494854d9c
Barun Singh <barunsingh@Baruns-MacBook-Pro.local>
2025-07-21 16:02:04 -0500

    Layout's

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const RootLayout = ({children}:{children:RootLayout}) => {
+  return (
+    <div className="root-layout">
+        <nav>
+            <Link href="/" className="flex items-center gap-2" >
+                <Image src="/logo.svg" alt = "Logo" width={38} height={32} />
+                <h2 className="text-primary-100">PrepWise</h2>
+            </Link>
+        </nav>
+        {children}
+    </div>
+  )
+}
+
+export default RootLayout
